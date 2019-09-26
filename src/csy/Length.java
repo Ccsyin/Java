cfdc5438aaa2dcdc958bb0597bef8a65e34172aa
Ccsyin <13507021302@163.com>
2019-09-26 20:23:37 +0800

    Character类的使用

---
+++ src/csy/Length.java
@@ -0,0 +1,11 @@
+package csy;
+
+import java.util.Scanner;
+
+public class Length {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String message = input.next();
+        System.out.println("The length of message is" + message.length());
+    }
+}
